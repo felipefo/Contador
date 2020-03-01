c0acb975712e8f6370ecfa165f7a0107dc549f2d
felipefo <felipefo@users.noreply.github.com>
2020-03-01 06:44:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var http = require('http');
 
+var porta = process.env.PORT || 8080;
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!');
-}).listen(PORT);
+}).listen(porta);
