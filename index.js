1d46b4b25b3972bdac3f275630f30637a8557069
felipefo <felipefo@users.noreply.github.com>
2020-03-02 06:42:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-var http = require('http');
 
 var porta = process.env.PORT || 8080;
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
-}).listen(porta);
+let http = require('http');
+let fs = require('fs');
+ 
+let handleRequest = (request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+	
+	console.log('hello world');
+	
+    fs.readFile('./index.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            respone.write('file not found');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+};
+ 
+http.createServer(handleRequest).listen(porta);
